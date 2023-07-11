c5fb41f23458daacc514fb929464d62e3eaf39d8
Omar Perez-Figueroa <137878729+OmarPerFig@users.noreply.github.com>
2023-07-11 10:59:48 +0200

    Define vector with each row of the matrix
    Playing with vectors.

--- matrix/matrix.cpp
+++ matrix/matrix.cpp
@@ -7,20 +7,22 @@ using namespace std;
 int main() {
  
  vector<vector<double>> M;
- M = vector<vector<double>>(4,vector<double>(4)); 
+ M = vector<vector<double>>(4,vector<double>(2)); 
 
  for (int i = 0; i < 4; i++) {
-  for (int j = 0; j < 4; j++) {
+  for (int j = 0; j < 2; j++) {
+   //M[i][j] = i + 1;
+   M[i][j] = 2*i + j + 1;
    cout << M[i][j] << '\n';
-   M[i][j] = 4*i + j + 1;
   }
  }
  
  cout << '\n';
 
  for (int i = 0; i < 4; i++) {
-  for (int j = 0; j < 4; j++) {
-   cout << M[i][j] << '\n';
+  vector<double> m = M[i];
+  for (int j = 0; j < 2; j++) {
+   cout << m[j] << '\n';
   }
  }
  
