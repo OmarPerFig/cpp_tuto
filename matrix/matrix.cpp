912f495a063e0beddcada0553dc12ab804437ea7
Omar Perez-Figueroa <137878729+OmarPerFig@users.noreply.github.com>
2023-07-11 15:13:27 +0200

    Separate func for real matrix, play w complex one
    Test a way of separating the real and imaginary parts of a complex matrix

--- matrix/matrix.cpp
+++ matrix/matrix.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int main() {
+void realmatrix() {
  
  vector<vector<double>> M;
  M = vector<vector<double>>(4,vector<double>(2)); 
@@ -23,6 +23,39 @@ int main() {
   vector<double> m = M[i];
   for (int j = 0; j < 2; j++) {
    cout << m[j] << '\n';
+  }
+ }
+ 
+}
+
+
+
+int main() {
+ 
+ vector<vector<complex<double>>> M;
+ M = vector<vector<complex<double>>>(4,vector<complex<double>>(2)); 
+ 
+ complex<double> imu(0,1); 
+ 
+ for (int i = 0; i < 4; i++) {
+  for (int j = 0; j < 2; j++) {
+   //M[i][j] = i + 1;
+   M[i][j] = 2.0*i + j + 1.0 + j*1.0*imu;
+   cout << M[i][j] << '\n';
+  }
+ }
+ 
+ cout << '\n';
+
+ for (int i = 0; i < 4; i++) {
+  vector<complex<double>> m = M[i];
+  vector<double> mreal(2); 
+  for (int k= 0; k < 2; k++) {
+	mreal[k] = m[k].real();
+	//*iter = m[k].real();
+  }
+  for (int j = 0; j < 2; j++) {
+   cout << mreal[j] << '\n';
   }
  }
  
